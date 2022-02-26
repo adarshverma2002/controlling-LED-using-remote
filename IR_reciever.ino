99dc44da1bd36b4a83caace54a635898634729cb
Adarsh Verma <73573533+adarshverma2002@users.noreply.github.com>
2022-02-26 14:04:15 +0530

    Add files via upload

---
+++ IR_reciever.ino
@@ -0,0 +1,21 @@
+#include <IRremote.h>
+
+int IRpin = 11;
+IRrecv irrecv(IRpin);
+decode_results results;
+
+void setup()
+{
+Serial.begin(9600);
+irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop()
+{
+if (irrecv.decode(&results))
+{
+Serial.println(results.value, DEC); // Print the Serial 'results.value'
+irrecv.resume(); // Receive the next value
+}
+Serial.println(results.value, DEC); // Print the Serial 'results.value'
+}
